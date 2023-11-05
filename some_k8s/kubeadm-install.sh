5352b3f733acca251cb4ca0553b81cbb5dfbf270
jinnonn <146999555+jinnonn@users.noreply.github.com>
2023-11-05 23:15:15 +0400

    Update kubeadm-install.sh

--- some_k8s/kubeadm-install.sh
+++ some_k8s/kubeadm-install.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 sudo apt-get update
 # apt-transport-https may be a dummy package; if so, you can skip that package
 sudo apt-get install -y apt-transport-https ca-certificates curl gpg
